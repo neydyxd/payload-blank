48607967a6eafcbb17156921e52428e30e72104c
Stepan Maksimov <sa.maksimov@team.globus-ltd.com>
2024-05-16 11:44:02 +0300

    test

---
+++ src/collections/Test.ts
@@ -0,0 +1,14 @@
+import { CollectionConfig } from 'payload/types'
+
+const Test: CollectionConfig = {
+    slug: 'test',
+    fields: [
+        {
+            name: 'name',
+            type: 'text'
+        }
+
+    ],
+}
+
+export default Test
